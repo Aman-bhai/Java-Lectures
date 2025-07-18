fb44ce14176513202b171d7c25fb236ba9a91a57
Aman Soni <amansoni18860@gmail.com>
2025-07-18 19:30:38 +0530

    First Program in Java

---
+++ firstProgram/src/firstProgram/FirstProgram.java
@@ -0,0 +1,11 @@
+package firstProgram;
+
+public class FirstProgram {
+
+	public static void main(String []args) {
+		
+		System.out.println("Hello World");
+		System.out.println();
+		System.out.println("I am learning Java Tutorial");
+	}
+}
