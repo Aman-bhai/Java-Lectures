1c43d3a69061f32697ae273ebaf9f9a4958b5e8c
Aman Soni <amansoni18860@gmail.com>
2025-07-18 20:34:03 +0530

    Varibles, datatypes and typecasting

---
+++ src/in/learning/java/typeCasting/TypeCasting.java
@@ -0,0 +1,35 @@
+package in.learning.java.typeCasting;
+
+public class TypeCasting {
+	public static void main(String []args) {
+//		TypeCating ---> implicit type casting and explit typecasting
+//		
+//		1) implicit typecasting ---> typcasting which is done automatically by the jvm 
+
+        int a=20;
+		long b=30;
+		
+		b=a;
+		System.out.println("Type Cast int to long : "+b);
+		
+		float c=3.0f;
+		double d=4;
+		
+		d=c;
+		System.out.println("Type Cast float to double : ");
+
+		System.out.println(c);
+		System.out.println(d);
+
+//		2) explicit typecasting ---> typcasting which is done manually or forcefully
+		
+//		c=d;  // error
+		
+		c=(float)d;
+		System.out.println("Type Cast double to float : "+c);
+
+
+		
+	}
+
+}
---
+++ src/in/learning/java/variable/Variables.java
@@ -0,0 +1,28 @@
+package in.learning.java.variable;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int a = 20;
+		System.out.println(a);
+
+		String name = "RamSagar";
+		System.out.println(name);
+
+		boolean b = true;
+		System.out.println(b);
+
+		// Data types
+
+		// 1 ) Primitive Datatypes
+		//		
+		//	  a) inbuilt with languages
+		//    b) 8 types (byte,short,int,long,char,float,double,boolean)
+
+		
+		//2) Non Primitive Data types/ userdefined datatypes/ reference data types
+			//
+			//a) createdby user
+			//b)array, String, List
+	}
+}
